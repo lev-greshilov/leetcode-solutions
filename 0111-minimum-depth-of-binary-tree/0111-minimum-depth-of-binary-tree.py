013a80eb5f5868f1d853d65203eaf74681244e8e
lev-greshilov <130522249+lev-greshilov@users.noreply.github.com>
2026-08-22 14:33:09 +0400

    0111-MINIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 34 ms (53.12%), Space: 44.3 MB (85.78%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -10,15 +10,16 @@ class Solution:
         if not root:
             return 0
 
+        isLeft = root.left
+        isRight = root.right
         # End node
-        isLeaf = not root.left and not root.right
-        if isLeaf:
+        if not isLeft and not isRight:
             return 1
             
         # Count min only in TRUE (not-None) branches
-        if root.left and not root.right:
+        if isLeft and not isRight:
             return self.minDepth(root.left) + 1
-        elif root.right and not root.left:
+        elif isRight and not isLeft:
             return self.minDepth(root.right) + 1
         else:
             return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
