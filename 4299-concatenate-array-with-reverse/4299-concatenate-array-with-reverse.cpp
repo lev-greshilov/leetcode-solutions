12db5d4f746b0d51731e071f0deadf707614575f
lev-greshilov <130522249+lev-greshilov@users.noreply.github.com>
2026-09-03 22:12:17 +0400

    4299-CONCATENATE-ARRAY-WITH-REVERSE Stats: Time: 0 ms (100%), Space: 30.2 MB (90.58%) - LeetHub

--- 4299-concatenate-array-with-reverse/4299-concatenate-array-with-reverse.cpp
+++ 4299-concatenate-array-with-reverse/4299-concatenate-array-with-reverse.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     vector<int> concatWithReverse(vector<int>& nums) {
         int n = nums.size();
+        int k = 0;
         std::vector<int> result(n * 2);
         for (int i = 0; i < n ; i++) {
-            result[i] = nums[i];
+            result[k++] = nums[i];
         }
-        for (int i = 0; i < n ; i++) {
-            result[n + i] = nums[(n - 1) - i];
+        for (int i = n-1; i >= 0 ; i--) {
+            result[k++] = nums[i];
         }
 
         return result;
