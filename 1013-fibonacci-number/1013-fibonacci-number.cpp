cd9f7392521a3f4d22521a30e3c811f70f167afc
lev-greshilov <130522249+lev-greshilov@users.noreply.github.com>
2026-09-08 16:38:08 +0400

    1013-FIBONACCI-NUMBER Stats: Time: 3 ms (64.48%), Space: 7.9 MB (38.22%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,7 +2,15 @@ class Solution {
 public:
     int fib(int n) {
         if (n == 0) return 0;
-        if (n < 3) return 1;
-        return fib(n - 1) + fib(n - 2);
+        int prev = 0;
+        int curr = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next; 
+        }
+ 
+        return curr;
     }
 };
