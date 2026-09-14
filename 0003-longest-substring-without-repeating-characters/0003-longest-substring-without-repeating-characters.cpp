b3eec571f1c14ff78a3d27e81ab8c7c86f80b35d
lev-greshilov <130522249+lev-greshilov@users.noreply.github.com>
2026-09-14 10:39:25 +0400

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 295 ms (23.09%), Space: 81.3 MB (23.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(std::string s) {
-        int max = 0;
+        int n = s.size();
+        int ans = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            std::string current_s = "";
-            int j = i;
-            
-            while (j < s.size()) {
-                if (current_s.find(s[j]) != std::string::npos) {
-                    break;
-                } else {
-                    current_s += s[j];
-                    if (max < current_s.size()) {
-                        max = current_s.size();
-                    }
-                }
-                j++;
+        unordered_set<char> st;
+
+        int left = 0;
+        int right = 0;
+
+        for (right = 0; right < n; right++) {
+            while (st.count(s[right])) {
+                st.erase(s[left]);
+                left++;
             }
+            st.insert(s[right]);
+            ans = max(ans, right - left + 1);
         }
-        return max;
+        return ans;
     }
 };
