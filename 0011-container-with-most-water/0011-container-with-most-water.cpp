f25f4aca4559b699ba7deac1f2d030e620abe9e6
lev-greshilov <130522249+lev-greshilov@users.noreply.github.com>
2026-09-15 19:44:17 +0400

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 3 ms (34.27%), Space: 62.9 MB (79.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -7,18 +7,24 @@ private:
 public:
     int maxArea(vector<int>& height) {
         int n = height.size();
-        int res = 0;
+        int res = INT_MIN;
 
+        int containerWidth = n - 1;
+        int containerHeight = 0;
+        
         int left = 0;
         int right = n - 1;
 
         while (left < right) {
-            res = max(res, getContainerArea(height, left, right)); 
+            containerHeight = min(height[left], height[right]);
+            res = max(res, containerWidth * containerHeight);
+
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
+            containerWidth--;
         }
         return res;
         
